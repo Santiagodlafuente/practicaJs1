6b3fd086c5e2e3c13709464af5ba0ec774adcb7a
Santiago Lafuente <104526730+Santiagodlafuente@users.noreply.github.com>
2023-08-28 15:14:36 -0300

    Create agrego puntos a hacer en app.js

---
+++ agrego puntos a hacer en app.js
@@ -0,0 +1,33 @@
+// practico1-Js
+/* Problema 1: Agregar y Eliminar Elementos​
+Crea un array vacío llamado frutas. Agrega tres frutas al final del array. Luego, elimina la primera fruta del array.​ */
+
+
+
+/* Problema 2: Actualización de Elementos​
+Tienes un array de nombres: ["Ana", "Juan", "María"]. Cambia el nombre "Juan" por "Pedro".​ */
+
+
+
+/* Problema 3: Reorganización de Elementos​
+Tienes un array de números: [10, 20, 30, 40, 50]. Mueve el último número al principio del array utilizando los métodos pop y unshift.​ */
+
+
+
+/* Problema 4: Creación de Subarray​
+Tienes un array de letras: ["a", "b", "c", "d", "e"]. Crea un nuevo array llamado subArray que contenga las letras "b", "c" y "d" utilizando el método slice. */
+
+
+
+/* Problema 5: Inversión de Array​
+Dado un array de números: [5, 10, 15, 20, 25], crea un nuevo array que contenga los mismos elementos pero en orden inverso utilizando el método reverse.​ */
+
+
+
+/* Problema 6: Extracción y Unión de Arrays​
+Tienes dos arrays: numeros1 con elementos [1, 2, 3] y numeros2 con elementos [4, 5, 6]. Utiliza los métodos pop y push para mover el último elemento de numeros1 al final de numeros2, y luego muestra el array numeros2.​ */
+
+
+
+/* Problema 7: Remover Elementos con Splice​
+Dado un array de frutas: ["manzana", "pera", "uva", "sandía", "banana"], utiliza el método splice para remover "uva" y "sandía" del array. Luego, muestra el array resultante. */
