39ca2d23d01f0392327bb2fa57ba820506669dbe
Santiago Lafuente <104526730+Santiagodlafuente@users.noreply.github.com>
2023-09-01 12:06:39 -0300

    complet app.js
    completado todos los puntos

--- practica4/app.js
+++ practica4/app.js
@@ -15,14 +15,14 @@
 
 //2-Contador Descendente: Imprime los números del 10 al 1 en orden descendente.
 
-// respuesta con while
+// respuesta 1 con while
 /* let num = 10;
 while (num > 0) {
     console.log(num);
     num--;
 } */
 
-//respuesta con for
+//respuesta 2 con for
 /* for( let num = 10; num > 0; num--){
     console.log(num);
 } */
@@ -31,25 +31,66 @@ while (num > 0) {
 
 //3-Suma de Números: Calcula la suma de los números del 1 al 10.
 
-/* let num = prompt('Ingrese un numero para saber sus tabla de multiplicar')
-for(let num  10; num > ){
-    console.log(`${num} x ${i} = ${num*i}`);
-    i++;
-}
-*/
+/* let num = parseInt(prompt('Ingrese un numero para saber sus sumas del 1 al 10.'));
 
-let suma = prompt('Ingrese un numero para saber sus sumas del 1 al 10');
+//RESULTADO 1
+for(let i = 1; i <= 10; i++ ){
+    console.log(`${num} + ${i} = ${num+i}`);
+};
+ */
 
+
+/*// RESULTADO 2  (no terminado)
 for (let i = 1; i <= 10; i++) {
-    console.log("La suma de los números del 1 al 10 es:", suma);
-    suma += i;
-}
+console.log('resultado de ' + suma + ' + ' + i + ' = ' + `${suma} + ${i}`);
+}; */
+
 
 
 //4-Tabla de Multiplicar: Pedir al usuario un numero y mostrar su tabla de multiplicar.
 
+/* let num = parseInt(prompt('Ingrese un numero para saber su tabla de multiplicar del 1 hasta 10'));
+
+//RESULTADO 1
+for(let i = 1; i <= 10; i++ ){
+    console.log(`${num} * ${i} = ${num*i}`);
+};  */
+
+// RESPUESTA 2
+
+
+
+
 //5-Contador Pares: Imprime los números pares del 2 al 20.
+
+/* // RESPUESTA 1
+ for (var i = 2; i <= 20; i += 2) {
+    console.log(i);
+}; */ 
+
+// RESPUESTA 2
+
+
 
 //6-Contador Impares: Imprime los números impares del 1 al 19.
 
+/* // RESPUESTA 1
+for (let i = 1; i <= 19; i += 2) {
+    console.log(i);
+}; */
+
+// RESPUESTA 2
+
+
+
 //7-Contador de Dígitos: Cuenta la cantidad de dígitos en un número dado.
+
+/* let num = parseInt(prompt("Ingrese un numero para saber cuantos digitos tiene." )); 
+
+//respuesta 1
+let numComoTexto = num.toString();
+let cantidadDigitos = numComoTexto.length;
+alert("El número " + num + " tiene " + cantidadDigitos + " dígitos.");
+ */
+
+//respuesta 2
